e3c36231befea4fcc621c3bd728f75121bcfaac8
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-26 11:55:26 +0700

    membuat env

--- services/komikService.js
+++ services/komikService.js
@@ -17,4 +17,52 @@ async function createKomik(database, komikData) {
     return newKomik;
 }
 
+async function getAllKomik(database) {
+    const komiks = await database.Komik.findAll();
 
+    return komiks.map(k => {
+        if (k.imageData) {
+            k.imageData = k.imageData.toString('base64');
+        }
+        return k;
+    });
+}
+
+async function getKomikById(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) throw new Error('Komik tidak ditemukan');
+
+    if (komik.imageData) {
+        komik.imageData = komik.imageData.toString('base64');
+    }
+
+    return komik;
+}
+
+async function updateKomik(database, id, komikData) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+
+    await komik.update(komikData);
+    return komik;
+}
+
+async function deleteKomik(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+
+    await komik.destroy();
+    return { message: `Komik dengan ID ${id} berhasil dihapus` };
+}
+
+module.exports = {
+    createKomik,
+    getAllKomik,
+    getKomikById,
+    updateKomik,
+    deleteKomik,
+};
